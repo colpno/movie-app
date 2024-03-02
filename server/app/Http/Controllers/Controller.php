b8c681732244e45c3f17804f3bcb55fbd0e4ef64
Colpno <gvinhh@gmail.com>
2024-03-02 19:04:05 +0700

    define some reuseable properties

--- server/app/Http/Controllers/Controller.php
+++ server/app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ApiResponse;
+use App\Services\Embedding;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -9,4 +11,13 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public $embedding;
+    public $apiResponse;
+
+    public function __construct()
+    {
+        $this->embedding = new Embedding();
+        $this->apiResponse = new ApiResponse();
+    }
 }
