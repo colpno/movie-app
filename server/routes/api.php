e16e3ea635ff0cb2dc9b5237d39fe5a303d493d8
Colpno <gvinhh@gmail.com>
2024-03-02 19:18:03 +0700

    integrate user routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -9,6 +9,12 @@ use Illuminate\Support\Facades\Route;
 Route::group(["prefix" => "v1"], function () {
 	Route::apiResource('favorites', FavoriteController::class);
 	Route::get('genres/{mediaType}', [GenreController::class, 'index'])->whereIn('mediaType', ['movie', 'tv']);
+	Route::controller(UserController::class)->prefix('users/{id}')->group(function () {
+		Route::get("/", 'show');
+		Route::put("/", 'update');
+		Route::patch("/", 'update');
+		Route::delete("/", 'destroy');
+	})->whereNumber('id');
 	Route::controller(VideoController::class)->prefix('{mediaType}')->group(function () {
 		Route::get("/", 'index');
 		Route::get("list/{type}", 'getByType')->whereIn('type', ['airing_today', 'popular', 'on_the_air', 'top_rated', 'now_playing', 'popular', 'top_rated', 'upcoming']);
