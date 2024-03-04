ab2c2d2ccd2e7af42e6b22b67ea8feea99975999
Colpno <gvinhh@gmail.com>
2024-03-04 23:30:23 +0700

    split routes into protected and public

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,25 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::group(['prefix' => "v1", 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
+	// Protected routes
+	Route::middleware('auth:sanctum')->group(function () {
+		Route::prefix("auth")->controller(AuthController::class)->group(function () {
+			Route::post('logout', 'logout');
+		});
+		Route::prefix('users/{id}')->controller(UserController::class)->group(function () {
+			Route::get("/", 'show');
+			Route::put("/", 'update');
+			Route::patch("/", 'update');
+			Route::delete("/", 'destroy');
+		})->whereNumber('id');
+		Route::apiResource('favorites', FavoriteController::class);
+	});
 
-Route::middleware('auth:sanctum')->prefix("v1")->namespace('App\Http\Controllers\Api\V1')->group(function () {
+	// Public routes
 	Route::prefix("auth")->controller(AuthController::class)->group(function () {
 		Route::post('register', 'register');
 		Route::post('login', 'login');
-		Route::post('logout', 'logout');
 	});
-
 	Route::get('genres/{mediaType}', [GenreController::class, 'index'])->whereIn('mediaType', ['movie', 'tv']);
-
-	Route::prefix('users/{id}')->controller(UserController::class)->group(function () {
-		Route::get("/", 'show');
-		Route::put("/", 'update');
-		Route::patch("/", 'update');
-		Route::delete("/", 'destroy');
-	})->whereNumber('id');
-
-	Route::apiResource('favorites', FavoriteController::class);
-
 	Route::prefix('{mediaType}')->controller(VideoController::class)->group(function () {
 		Route::get("/", 'index');
 		Route::get("list/{type}", 'getByType')->whereIn('type', ['airing_today', 'popular', 'on_the_air', 'top_rated', 'now_playing', 'popular', 'top_rated', 'upcoming']);
