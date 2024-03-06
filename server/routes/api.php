32cfe271c77b9a56e0039a1ba62c1fe4f9678d83
Colpno <gvinhh@gmail.com>
2024-03-06 14:27:58 +0700

    enforce restriction in each route individually instead of group

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,33 +1,36 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\FavoriteController;
+use App\Http\Controllers\Api\V1\GenreController;
+use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\Api\V1\VideoController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => "v1", 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
-	// Protected routes
-	Route::middleware('auth:sanctum')->group(function () {
-		Route::prefix("auth")->controller(AuthController::class)->group(function () {
-			Route::post('logout', 'logout');
-		});
-		Route::prefix('users/{id}')->controller(UserController::class)->group(function () {
-			Route::get("/", 'show');
-			Route::put("/", 'update');
-			Route::patch("/", 'update');
-			Route::delete("/", 'destroy');
-		})->whereNumber('id');
-		Route::apiResource('favorites', FavoriteController::class);
-	});
-
+Route::group(['prefix' => "v1"], function () {
 	// Public routes
+	Route::get('genres/{mediaType}', [GenreController::class, 'index'])->whereIn('mediaType', ['movie', 'tv']);
 	Route::prefix("auth")->controller(AuthController::class)->group(function () {
 		Route::post('register', 'register');
 		Route::post('login', 'login');
 	});
-	Route::get('genres/{mediaType}', [GenreController::class, 'index'])->whereIn('mediaType', ['movie', 'tv']);
 	Route::prefix('{mediaType}')->controller(VideoController::class)->group(function () {
-		Route::get("/", 'index');
-		Route::get("list/{type}", 'getByType')->whereIn('type', ['airing_today', 'popular', 'on_the_air', 'top_rated', 'now_playing', 'popular', 'top_rated', 'upcoming']);
-		Route::get("trending/{time}", 'getTrending')->whereIn('time', ['day', 'week']);
-		Route::get("search", 'search');
-		Route::get("{id}", 'detail')->whereNumber('id');
-	})->whereIn('mediaType', ['movie', 'tv']);
+		Route::get("/", 'index')->whereIn('mediaType', ['movie', 'tv']);
+		Route::get("search", 'search')->whereIn('mediaType', ['movie', 'tv']);
+		Route::get("{id}", 'detail')->whereIn('mediaType', ['movie', 'tv'])->whereNumber('id');
+	});
+
+	// Protected routes
+	Route::middleware('auth:sanctum')->group(function () {
+		Route::apiResource('favorites', FavoriteController::class);
+		Route::prefix("auth")->controller(AuthController::class)->group(function () {
+			Route::post('logout', 'logout');
+		});
+		Route::prefix('users/{id}')->controller(UserController::class)->group(function () {
+			Route::get("/", 'show')->whereNumber('id');
+			Route::put("/", 'update')->whereNumber('id');
+			Route::patch("/", 'update')->whereNumber('id');
+			Route::delete("/", 'destroy')->whereNumber('id');
+		});
+	});
 });
