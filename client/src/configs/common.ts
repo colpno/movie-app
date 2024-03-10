c0d9fe8bdcd0a915d8fc1d6486debdd87daaadfe
Colpno <gvinhh@gmail.com>
2024-03-10 13:56:50 +0700

    add app configs

---
+++ client/src/configs/common.ts
@@ -0,0 +1,3 @@
+export const APP_URL = import.meta.env.VITE_APP_URL ?? 'http://localhost:5173';
+export const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:8000/api/v1';
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL ?? 'http://localhost:8000';
