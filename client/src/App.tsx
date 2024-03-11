7b629d01044b7ca01b4939d45c4d4470673d4c0f
Colpno <gvinhh@gmail.com>
2024-03-11 13:48:04 +0700

    add providers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,19 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RouterProvider } from 'react-router-dom';
+
+import Toast from './components/Toast.tsx';
+import queryClient from './lib/react-query/client.ts';
+import { routes } from './routes/index.tsx';
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={routes} />
+      <Toast />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
