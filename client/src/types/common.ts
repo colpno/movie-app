7d7d12c132b4a2a32ea49b427669c12907df08b6
Colpno <gvinhh@gmail.com>
2024-03-15 01:29:41 +0700

    add trailer type

--- client/src/types/common.ts
+++ client/src/types/common.ts
@@ -31,6 +31,8 @@ export interface User {
   updated_at: Date;
 }
 
+export type Video = DiscoverMovie | DiscoverTV | Movie | TV;
+
 export interface Movie {
   adult: boolean;
   backdrop_path?: string;
@@ -222,4 +224,17 @@ export interface SearchTV {
   name: string;
   vote_average: number;
   vote_count: number;
+}
+
+export interface Trailer {
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  key: string;
+  site: string;
+  size: number;
+  type: string;
+  official: boolean;
+  published_at: string;
+  id: string;
 }
