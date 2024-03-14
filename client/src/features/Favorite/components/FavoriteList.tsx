f5807369cb8a00f39d69eae928a8a147705640c8
Colpno <gvinhh@gmail.com>
2024-03-14 17:38:29 +0700

    optimize code

--- client/src/features/Favorite/components/FavoriteList.tsx
+++ client/src/features/Favorite/components/FavoriteList.tsx
@@ -1,24 +1,15 @@
-import { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 import Card from '~/components/Card/Card.tsx';
-import { Favorite } from '~/types/common.ts';
 import { Loader } from '../loader.ts';
 
 function FavoriteList() {
-  const { favorites } = useLoaderData() as Loader;
-  const [videos, setVideos] = useState<Favorite['video_id'][] | never[]>([]);
-
-  useEffect(() => {
-    if (favorites && favorites.length > 0) {
-      setVideos(favorites.map((favorite) => favorite.video_id));
-    }
-  }, [favorites]);
+  const { favorites = [] } = useLoaderData() as Loader;
 
   return (
     <div className="flex flex-wrap gap-4">
-      {videos.map((video) => {
-        return <Card data={video} key={video.id} />;
+      {favorites.map((favorite) => {
+        return <Card data={favorite.video_id} key={favorite.id} favorite={favorite} />;
       })}
     </div>
   );
