6df79512b64ce086a32a238445604dadc69e6e17
Colpno <gvinhh@gmail.com>
2024-03-16 15:06:49 +0700

    redirect to home page after logging out

--- client/src/apis/auth/logout.ts
+++ client/src/apis/auth/logout.ts
@@ -1,9 +1,12 @@
 import { useMutation } from '@tanstack/react-query';
 
+import { path } from '~/constants/routes.ts';
 import queryClient from '~/lib/react-query/client.ts';
+import { router } from '~/routes/routes.tsx';
 import { emitToast } from '~/utils/toast.ts';
 import axiosClient, { ApiSuccessResponse } from '../axios.ts';
 import { userKeys } from '../user/queryKey.ts';
+import authKeys from './queryKey.ts';
 
 export interface UseLogoutResponse extends ApiSuccessResponse {
   message: string;
@@ -17,10 +20,12 @@ const logout = () => {
 export const useLogout = () =>
   useMutation({
     mutationFn: logout,
+    mutationKey: authKeys.logout,
     onSuccess: ({ message }) => {
       emitToast(message, 'success');
       queryClient.removeQueries({
         queryKey: userKeys.detail,
       });
+      router.navigate(path.HOME);
     },
   });
