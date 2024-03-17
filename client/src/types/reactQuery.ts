2c422090d5ca21ed64c0aaaba642dab784e632bc
Colpno <gvinhh@gmail.com>
2024-03-17 20:32:42 +0700

    add type of infinite query options

--- client/src/types/reactQuery.ts
+++ client/src/types/reactQuery.ts
@@ -1,4 +1,4 @@
-import { QueryFunctionContext, QueryKey } from '@tanstack/react-query';
+import { QueryFunctionContext, QueryKey, UseInfiniteQueryOptions } from '@tanstack/react-query';
 
 export type InfiniteQueryFunctionParams<T extends QueryKey = QueryKey> = QueryFunctionContext<
   T,
@@ -6,3 +6,26 @@ export type InfiniteQueryFunctionParams<T extends QueryKey = QueryKey> = QueryFu
 >;
 
 export type QueryFunctionParams<T extends QueryKey = QueryKey> = QueryFunctionContext<T>;
+
+export type InfiniteQueryOptions<T> = Partial<
+  Omit<
+    UseInfiniteQueryOptions<T>,
+    | 'queryKey'
+    | 'queryFn'
+    | 'getNextPageParam'
+    | 'getPreviousPageParam'
+    | 'behavior'
+    | 'initialData'
+    | 'persister'
+    | 'placeholderData'
+    | 'refetchInterval'
+    | 'refetchIntervalInBackground'
+    | 'refetchOnMount'
+    | 'refetchOnReconnect'
+    | 'refetchOnWindowFocus'
+    | 'select'
+    | 'throwOnError'
+  >
+> & {
+  queryKey?: unknown[];
+};
