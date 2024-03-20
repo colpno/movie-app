cf0834b744601ddbf4b780bd20b8fa0111369849
Colpno <gvinhh@gmail.com>
2024-03-20 17:45:04 +0700

    add loading screen during logging out

--- client/src/components/Navbar/components/NavbarLogout.tsx
+++ client/src/components/Navbar/components/NavbarLogout.tsx
@@ -2,12 +2,15 @@ import { FaPowerOff } from 'react-icons/fa';
 
 import { useLogout } from '~/apis/auth/logout.ts';
 import { userKeys } from '~/apis/user/queryKey.ts';
+import useLoading from '~/hooks/useLoading.ts';
 import queryClient from '~/lib/react-query/client.ts';
 
 function NavbarLogout() {
-  const { mutate: logout } = useLogout();
+  const { mutate: logout, isPending } = useLogout();
   const user = queryClient.getQueryData(userKeys.detail);
   const isLoggedIn = Boolean(user);
+
+  useLoading(isPending);
 
   if (!isLoggedIn) return <></>;
 
